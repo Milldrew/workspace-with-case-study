c812adf17883d32098af099f9b7bf18495e6a833
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-11-24 19:27:00 -0500

    📅  | Thu Nov 24 19:27:00 EST 2022

---
+++ src/app/jumbotron/park/park.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '.milldrew-park',
+  templateUrl: './park.component.html',
+  styleUrls: ['./park.component.scss'],
+})
+export class ParkComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
