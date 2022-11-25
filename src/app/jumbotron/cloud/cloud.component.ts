37c9845d502e1636387c3d755615ad3dee428177
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-11-24 19:46:27 -0500

    📅  | Thu Nov 24 19:46:27 EST 2022

--- src/app/jumbotron/cloud/cloud.component.ts
+++ src/app/jumbotron/cloud/cloud.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'milldrew-cloud',
+  selector: '.milldrew-cloud',
   templateUrl: './cloud.component.html',
-  styleUrls: ['./cloud.component.scss']
+  styleUrls: ['./cloud.component.scss'],
 })
 export class CloudComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
