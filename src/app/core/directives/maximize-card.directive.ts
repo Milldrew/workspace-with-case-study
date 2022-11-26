ba71aea219275c0316acd7471fcf27bd11682cc8
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-11-25 21:39:13 -0500

    📅  | Fri Nov 25 21:39:13 EST 2022

--- src/app/core/directives/maximize-card.directive.ts
+++ src/app/core/directives/maximize-card.directive.ts
@@ -2,15 +2,32 @@ import * as d3 from 'd3';
 import { Input, Directive } from '@angular/core';
 
 @Directive({
-  selector: '[milldrewMaximizeCard]',
+  selector: '[maximizeCard]',
 })
 export class MaximizeCardDirective {
   @Input()
   cardId: string;
 
   cardSelection: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
+  parentSVGWidth: string;
 
   ngAfterViewInit() {
     this.cardSelection = d3.select('#' + this.cardId);
+    console.log(this.cardSelection);
+    this.maximizeCardOnClick();
+    this.parentSVGWidth = d3.select('#parent-svg').style('width');
+    this.cardSelection.style('width', this.cardSelection.style('width'));
+  }
+
+  maximizeCardOnClick() {
+    this.cardSelection.on('click', () => {
+      this.maximizeAnimation();
+    });
+  }
+  maximizeAnimation() {
+    const maximize = this.cardSelection.transition('maximize');
+    maximize.duration(1000);
+    maximize.ease(d3.easeLinear);
+    maximize.style('width', this.parentSVGWidth);
   }
 }
