b03dd23dcb0776e1bd273189d60ff800f98cf19a
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-11-26 08:32:39 -0500

    📅  | Sat Nov 26 08:32:39 EST 2022

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewStateService } from '../core/services/view-state.service';
 
 @Component({
   selector: 'milldrew-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  constructor(public viewStateService: ViewStateService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
