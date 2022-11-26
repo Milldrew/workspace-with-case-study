6a16c504e96ae5ba689539a622841c4052b367b6
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-11-26 08:31:57 -0500

    📅  | Sat Nov 26 08:31:57 EST 2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ViewStateService } from './core/services/view-state.service';
 
 @Component({
   selector: 'milldrew-root',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(public viewStateService: ViewStateService) {}
 }
