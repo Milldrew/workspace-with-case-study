d4028d8dfd82a07dcf22b2f1dd436f0f8831bbdb
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-11-26 10:43:04 -0500

    📅  | Sat Nov 26 10:43:04 EST 2022

--- src/app/core/directives/maximize-card.directive.ts
+++ src/app/core/directives/maximize-card.directive.ts
@@ -8,6 +8,8 @@ export class MaximizeCardDirective {
   @Input()
   cardId: string;
 
+  isMaximixed = false;
+  originalSize: string;
   cardSelection: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
   parentSVGWidth: string;
 
@@ -16,18 +18,42 @@ export class MaximizeCardDirective {
     console.log(this.cardSelection);
     this.maximizeCardOnClick();
     this.parentSVGWidth = d3.select('#parent-svg').style('width');
-    this.cardSelection.style('width', this.cardSelection.style('width'));
+    this.originalSize = this.cardSelection.style('width');
+    this.cardSelection.style('width', this.originalSize);
   }
 
   maximizeCardOnClick() {
     this.cardSelection.on('click', () => {
-      this.maximizeAnimation();
+      if (this.isMaximixed) {
+        this.minimizeAnimation();
+      } else {
+        this.maximizeAnimation();
+      }
     });
+  }
+  maximizeCardAnimation() {
+    const maximize = this.cardSelection.transition('maximize');
+    maximize.duration(1000);
+    maximize.ease(d3.easeLinear);
+    maximize.style('width', this.parentSVGWidth);
   }
   maximizeAnimation() {
     const maximize = this.cardSelection.transition('maximize');
     maximize.duration(1000);
     maximize.ease(d3.easeLinear);
     maximize.style('width', this.parentSVGWidth);
+    maximize.on('end.maximize', () => {
+      this.isMaximixed = true;
+    });
+  }
+
+  minimizeAnimation() {
+    const minimize = this.cardSelection.transition('minimize');
+    minimize.duration(1000);
+    minimize.ease(d3.easeLinear);
+    minimize.style('width', this.originalSize);
+    minimize.on('end.minimize', () => {
+      this.isMaximixed = true;
+    });
   }
 }
